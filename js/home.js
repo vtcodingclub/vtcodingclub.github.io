1e96a5f33cb9e381729d1990b5e24f9f60e9b85a
starrydash <ryclowe@gmail.com>
2022-05-04 16:02:18 -0700

    Revert "Redoing Changes v2"
    This reverts commit 38d32315e738e25604f86b13edab9c5843910b87, reversing
    changes made to 069c147c6d5de99dd6c29285d8a2446a70397396.

--- js/home.js
+++ js/home.js
@@ -1,18 +1,17 @@
-@ -1,26 +0,0 @@
-    function burger_button() {
-        if (document.getElementById("hamburger_bar1").style.display === "none") {
-            document.getElementById("hamburger_bar1").style.display = "inline-block";
-            document.getElementById("hamburger_bar2").style.display = "inline-block";
-            document.getElementById("hamburger_bar3").style.display = "inline-block";
-            var listener = window.matchMedia("(min-width: 700px)")
-            Hider(listener)
-            listener.addListener(Hider)
-        } else {
-            document.getElementById("hamburger_bar1").style.display = "none";
-            document.getElementById("hamburger_bar2").style.display = "none";
-            document.getElementById("hamburger_bar3").style.display = "none";
-        }
+function burger_button() {
+    if (document.getElementById("hamburger_bar1").style.display === "none") {
+        document.getElementById("hamburger_bar1").style.display = "inline-block";
+        document.getElementById("hamburger_bar2").style.display = "inline-block";
+        document.getElementById("hamburger_bar3").style.display = "inline-block";
+        var listener = window.matchMedia("(min-width: 700px)")
+        Hider(listener)
+        listener.addListener(Hider)
+    } else {
+        document.getElementById("hamburger_bar1").style.display = "none";
+        document.getElementById("hamburger_bar2").style.display = "none";
+        document.getElementById("hamburger_bar3").style.display = "none";
     }
+}
 
 function Hider(x) {
     if (x.matches) {
