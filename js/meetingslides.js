332c75a34dd33501ee389d71510c222974a14766
starrydash <ryclowe@gmail.com>
2022-05-16 15:55:17 -0700

    Added iframe_urls

--- js/meetingslides.js
+++ js/meetingslides.js
@@ -7,99 +7,106 @@ slides = [
         "slide_name": "May 11th, 2022 Meeting Slides",
         "about_slide": "May 11th, 2022",
         "type": "general",
-        "slide_url": "https://docs.google.com/presentation/d/1_uEJYcfGosJzlOp6nVbC0YzPFQWLSDL4TJWeus7G32A/edit?usp=sharing",
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1pmI0EHo4kmgLg4O7EWHSmmRRTDOu9Xllh7oEwxHmXVs/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vSjDqDePRLF3K4D-5Dq_dsiuS-qAdKRFl-z47zdGH8hXOOO8agP_R1Ot3dhdHQM8y-wHGtAX-NghdBI/embed?"
     },
     {
         "slide_name": "How to Code and Deploy your own Website",
         "about_slide": "April 27th - May 11th 2022",
         "type": "workshop",
-        "slide_url": "https://docs.google.com/presentation/d/1Ke-Ij26hk8IxkDGQmqK4hYYjBOn3iASvIgjm-yBOFJo/edit?usp=sharing,
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1Ke-Ij26hk8IxkDGQmqK4hYYjBOn3iASvIgjm-yBOFJo/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vTdojaZdiU_fcHboYQV1tEKDLlq4dJMvvqZFq2q1Q4CY4JXkOZqntU8dsGmbiD0NUI1WoLEnCjpiSyW/embed?"
     },
     {
         "slide_name": "Creating a Discord Bot in Python",
         "about_slide": "April 6th - 13th 2022",
         "type": "workshop",
-        "slide_url": "https://docs.google.com/presentation/d/1_uEJYcfGosJzlOp6nVbC0YzPFQWLSDL4TJWeus7G32A/edit?usp=sharing",
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1av-kSI712acKLr3fixp80rZPeK_JxDKU8raxj_tI_uc/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vR5KkCTBFJ-y_n1QQbn8jHNDt6DLBryaBM-SJwdulwpyanJF9wT7JDS4zT7qO2Aqu3Ipez4LrP0V8uO/embed?"
     },
     {
         "slide_name": "March 30th, 2022 Meeting Slides",
         "about_slide": "March 30th 2022",
         "type": "general",
-        "slide_url": "https://docs.google.com/presentation/d/1_uEJYcfGosJzlOp6nVbC0YzPFQWLSDL4TJWeus7G32A/edit?usp=sharing",
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1-7YvoJ4zNAxUp2htEBV05hP7puml9yZUxR_f0vaR97A/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vSsa-CZ36RoP24Lmos2T3DPP9vfLBR6Hv5ldmJQJwTBhOQtCgVoRgMkLHqsyppDwJvcGXROz_EvkS0o/embed?"
     },
     {
         "slide_name": "March 2nd - 9th, 2022: Intro to Web Development Part 2",
         "about_slide": "March 2nd - 9th, 2022",
         "type": "workshop",
-        "slide_url": "https://docs.google.com/presentation/d/1_uEJYcfGosJzlOp6nVbC0YzPFQWLSDL4TJWeus7G32A/edit?usp=sharing",
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1zZsnFG7KObOTpS1BGXJDm5xRgUW7gLJtZ58cxpK5fHM/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vRE-7hm09SrGf8IAxkTptTNZi8Agy-Kqkoz6_xvPfQJCas3MhyBzTtIL40Azv9ZCGbjN16p-eE0qUHc/embed?"
     },
     {
         "slide_name": "March 9th, 2022 Meeting Slides",
         "about_slide": "March 9th, 2022",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/1zZsnFG7KObOTpS1BGXJDm5xRgUW7gLJtZ58cxpK5fHM/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vRE-7hm09SrGf8IAxkTptTNZi8Agy-Kqkoz6_xvPfQJCas3MhyBzTtIL40Azv9ZCGbjN16p-eE0qUHc/embed?"
     },
     {
         "slide_name": "February 16th, 2022 Meeting Slides",
         "about_slide": "February 16th, 2022",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/1lsp9RRZoBBkm6zN2tQE9ez4mj3OR1LWsqGSsTrl71pY/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vS3-Rby58HJQhY5QpRDCofcIs00xhF6_OMDCx84-pCZ93iaEiu2qj_V-WQF3Fyj_8-AF3okgFl9uwip/embed?"
     },
     {
         "slide_name": "Minecraft Python",
         "about_slide": "February 9th - 16th, 2022",
         "type": "workshop",
         "slide_url": "https://docs.google.com/presentation/d/1UiHB8k8XqDRFNivfwIIFrs8MM69oKsOypQb4V8oxItQ/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vTvsEA8XKB2j424uRiPt2blUNKzgzG51w8gHPvjAvbCbxDUwI1duOLAZLA_0QtRVPsIy6w0TfOoeE0q/embed?"
     },
     {
         "slide_name": "Jan 19th - Feb 2nd 2022: Game Development with JavaScript Workshop",
         "about_slide": "January 19th - February 2nd, 2022",
         "type": "workshop",
         "slide_url": "https://docs.google.com/presentation/d/1qDopcyFgYHjud97lVSN0rbFW9TGTbgmNjqAFinW70L8/edit?usp=sharing",
-        "iframe_url": ""
-    }
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vQTfIk2VUBWnu9FlGUQApvaP6NY9x5IEHa0EsVClooCzCvUjXlsNnwzOO5wY_db2sYaycXUgmyrB3zF/embed?"
+    },
     {
         "slide_name": "January 12th, 2022 Meeting Slides",
         "about_slide": "January 12th, 2022",
         "type": "general",
-        "slide_url": " https://docs.google.com/presentation/d/1aD-LBRE1YPwBet-T3DhaD3vc8y37ivYVTTwFipKguac/edit?usp=sharing",
-        "iframe_url": ""
+        "slide_url": "https://docs.google.com/presentation/d/1aD-LBRE1YPwBet-T3DhaD3vc8y37ivYVTTwFipKguac/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vQEDqVj3Ef3dXaD2veyHGrGwB-lEj0VK1hNdGo8GhV5-ZlqY61l6YTJ7RJT7_n2szQp2eBwl_AGbHkw/embed?"
     },
     {
         "slide_name": "Dec 15th, 2021 Meeting Slides",
         "about_slide": "December 15th, 2021",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/1elwG4Na4FYDCdKvSWqYlK_kV2M1yU47UkWX7kDUJii8/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vT7RztrWvweUzPFx7w8ryAMKH8FHow9osnPvKuALB7kzarG3w87V_hElX40NrgIU0jbCTURcHoe49v3/embed?"
     },
     {
         "slide_name": "Dec 1st, 2021 Meeting Slides",
         "about_slide": "December 1st, 2021",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/1L_BPDgIoNcaQjhK1wyEkxgDI4FsH5SLV38rhptbq7sw/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vR8yx-jeeOnQMNPIHkn-eqJsJtCYvQsuz3ThouaClWou9blMY7sKof4LdVme0uFrThYr418V5fAYC9I/embed?"
     },
     {
         "slide_name": "Python Introduction",
         "about_slide": "November 17th, 2021",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/16siK8XkoZdYFCR_PaQ5CjwRBA3uPUcvlI_Kgi314ydc/edit?usp=drivesdk",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vRbGnuEa1avhxb_Y0zTWhb2HqaMhjbKE5oVHbNGjj9W0c2fOhg78kfSo2W5f54HzRdjU1ay-Gmo0NbE/embed?"
     },
     {
         "slide_name": "Nov 10th, 2021 Meeting Slides",
         "about_slide": "November 10th, 2021",
         "type": "general",
         "slide_url": "https://docs.google.com/presentation/d/1cLQ9vf3aGZezeOEbM2P-7cJm8fmQpxpNP6hJKV1-RiY/edit?usp=sharing",
-        "iframe_url": ""
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vSxS-P2zQCbwn-NiHFthefjmqcgGhTENlUEvgps9K2vkxbjfZ40q7CouISTqV7g-j8t3LpCQHHFyHsE/embed?"
+    },
+    {
+        "slide_name": "Nov 3rd, 2021 Meeting Slides",
+        "about_slide": "November 3rd, 2021",
+        "type": "general",
+        "slide_url": "https://docs.google.com/presentation/d/1u6le4X9WY9nwCS9TOdgBIjly9fZPbIJNld57kv7ebr8/edit?usp=sharing",
+        "iframe_url": "https://docs.google.com/presentation/d/e/2PACX-1vTAgWS4Waluqge5hjn8UbVTs-2riRipeYAH8YAtnq1RI0GDQ_9wN6Klecm8i9pHFumQKvvLTFmQPJqg/embed?"
     },
   {
       "slide_name": "Intro to Web Development Part 1",
